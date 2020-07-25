2930ec23150ef415f27dd472ff1a70aa563d140b
Vinicius Cavalcanti <vhpcavalcanti@outlook.com>
2020-07-25 19:09:52 -0300

    showing the help menu with the banner and usages

--- covid.sh
+++ covid.sh
@@ -14,32 +14,39 @@ banner()
 
 usage()
 {
-  banner
-  echo "Usage: $0 [Options]:
-    -a, --list-all    List all countries
-    -c, --country     Specific a country
-    -h, --help        Help menu
-    -n, --no-banner   Hide banner
+  echo "usage: $0 [options]:
+    -a, --list-all    list statics for all countries
+    -c, --country     list statics for a specific country
+    -h, --help        open the help menu
+    -n, --no-banner   hide the /covid19/ banner
     "
 }
 
 main()
 {
-  if [ $# -lt 1 ]; then
+  if [ "$listall" == true ]; then
+    banner
+    res=$(curl -X GET "https://disease.sh/v3/covid-19/countries" -H "accept: application/json")
+    echo $res | jq -r '(["Country", "Cases", "Deaths", "Recovered"] | (., map(length*"-"))), (.[] | [.country, .cases, .deaths, .recovered]) | @csv' | column -t -s ","
+  elif [ "$help" == true ]; then
+    banner
     usage
   fi
-
-  if [ "$listall" == true ]; then
-      alldata=$(curl -X GET "https://disease.sh/v3/covid-19/countries" -H "accept: application/json")
-      echo $alldata | jq -r '(["Country", "Cases", "Deaths", "Recovered"] | (., map(length*"-"))), (.[] | [.country, .cases, .deaths, .recovered]) | @csv' | column -t -s ","
-  fi
 }
+
+if [ $# -eq 0 ]; then
+  usage
+fi
 
 while test $# -gt 0; do
   case "$1" in
     -a|--list-all)
     shift
     listall=true
+    ;;
+    -h|--help)
+    shift
+    help=true
     ;;
     -n|--no-banner)
     shift
