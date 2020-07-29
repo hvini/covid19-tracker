d7dcb8a989131fc1dd3960769a7ace543a6da37a
Vinicius Cavalcanti <vhpcavalcanti@outlook.com>
2020-07-28 23:20:00 -0300

    sparkline representation for no. cases, deaths and recovered for last 30 days data

--- src/covid.sh
+++ src/covid.sh
@@ -27,11 +27,12 @@ or to make digital or hard copies of part or all of this work is subject to the 
 https://github.com/hvini/covid19-tracker
 
 common options:
-  -a, --list-all    list statistics for all countries
-  -c, --country     list statistics for an specific country
-  -g, --global      list global statistics
-  -h, --help        open the help menu
-  -n, --no-banner   hide the /covid19/ banner
+  -a,  --list-all      list all countries statistics for today and yesterday
+  -c,  --country       list statistics for an specific country
+  -g,  --global        list global statistics for today and yesterday
+  -h,  --help          open the help menu
+  -hI, --historical    list global historical { last 30 days } statistics
+  -n,  --no-banner     hide the /covid19/ banner
 examples:
   ./covid.sh -c brazil
   ./covid.sh --global
@@ -48,7 +49,7 @@ main()
 {
   url="https://disease.sh/v3/covid-19"
 
-  if [ "$listall" == true ] && [ ! -z "$country" ]; then
+  if [ "$listall" == true ] && [ -n "$country" ]; then
     err "list all and country cannot be mixed!"
 
   elif [ "$listall" == true ]; then
@@ -58,7 +59,7 @@ main()
     echo ""
     echo $res | jq -r '(["country", "cases", "deaths", "recovered"] | (., map(length*"-"))), (.[] | [.country, .cases, .deaths, .recovered]) | @csv' | column -t -s ","
   
-  elif [ -n "$country" ]; then
+  elif [ -n "$country" ] && [ -z "$historical" ]; then
     banner
     echo "country: $country"
     echo ""
@@ -66,6 +67,34 @@ main()
     echo ""
     echo $res | jq -r '(["country", "cases", "deaths", "recovered"] | (., map(length*"-"))), ([.country, .cases, .deaths, .recovered]) | @csv' | column -t -s ","
   
+  elif [ -z "$country" ] && [ "$historical" == true ]; then
+    banner
+    printf "global\n\n"
+
+    res=$(curl --progress-bar -X GET "$url/historical/all" -H "accept: application/json")
+
+    cases=$(echo $res | jq -r '.cases' | jq '.[]')
+    deaths=$(echo $res | jq -r '.deaths' | jq '.[]')
+    recovered=$(echo $res | jq -r '.recovered' | jq '.[]')
+
+    printf "\ncases: $(spark ${cases})\n\n"
+    printf "deaths: $(spark ${deaths})\n\n"
+    printf "recovered: $(spark ${recovered})\n\n"
+
+  elif [ -n "$country" ] && [ "$historical" == true ]; then
+    banner
+    printf "country: ${country}\n\n"
+
+    res=$(curl --progress-bar -X GET "$url/historical/$country" -H "accept: application/json")
+    
+    cases=$(echo $res | jq -r '.timeline.cases' | jq '.[]')
+    deaths=$(echo $res | jq -r '.timeline.deaths' | jq '.[]')
+    recovered=$(echo $res | jq -r '.timeline.recovered' | jq '.[]')
+
+    printf "\ncases: $(spark ${cases})\n\n"
+    printf "deaths: $(spark ${deaths})\n\n"
+    printf "recovered: $(spark ${recovered})\n\n"
+
   elif [ "$help" == true ]; then
     banner
     usage
@@ -73,11 +102,14 @@ main()
   elif [ "$global" == true ]; then
     banner
     echo ""
-    res=$(curl --fail --progress-bar -X GET "$url/all" -H "accept: application/json")
+    res=$(curl --progress-bar -X GET "$url/all" -H "accept: application/json")
     echo ""
     echo $res | jq -r '(["cases", "deaths", "recovered"] | (., map(length*"-"))), ([.cases, .deaths, .recovered]) | @csv' | column -t -s ","
   fi
 }
+
+basedir=$( cd `dirname $0`; pwd )
+source ${basedir}/libs/spark
 
 check_dependencies
 
@@ -98,6 +130,9 @@ while test $# -gt 0; do
     ;;
     -h|--help)
     help=true
+    ;;
+    -hI|--historical)
+    historical=true
     ;;
     -n|--no-banner)
     nobanner=true
